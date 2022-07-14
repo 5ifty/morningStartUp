2c10e9ff9ce5325c8a1ddc816858d3a7993be464
Harry <5iftyy666@gmail.com>
2022-07-14 14:32:51 +0100

    Commented out clunky string which is only used for debugging

--- utils/main.py
+++ utils/main.py
@@ -4,7 +4,8 @@ import time
 
 def Batch():
     batchcli = subprocess.Popen(r"C:\Users\oem\Documents\Recent Code\Startup\utils\bat.bat")
-    print(subprocess.check_output(r"C:\Users\oem\Documents\Recent Code\Startup\utils\bat.bat"))
+    # print(subprocess.check_output(r"C:\Users\oem\Documents\Recent Code\Startup\utils\bat.bat"))
+    # This is used for debugging the cmd script
     time.sleep(5)
     batchcli.kill()
 
